6c8c0818170439615dcac18b8a6d08f0a6fa7eff
Premkumar <pkatcoding@gmail.com>
2025-12-26 11:00:30 +0530

    Uploaded DB_Engine File

---
+++ DB_Engine.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus
+
+# Credentials
+USER = "postgres"
+PASSWORD = "Red@Mango474"   
+HOST = "db.mmshlebwrugaowdoiqdh.supabase.co"
+PORT = 5432
+DBNAME = "postgres"
+
+TABLE_NAME = "people"
+CSV_FILE_PATH = "stabiliser_data.csv"
+
+# URL encode password, in case it has special characters
+ENCODED_PASSWORD = quote_plus(PASSWORD)
+
+# Create engine
+engine = create_engine(
+    f"postgresql+psycopg2://{USER}:{ENCODED_PASSWORD}@{HOST}:{PORT}/{DBNAME}"
+)
